01c924f83f94e39412b7db3b6e07569aef43798f
Fluorky <mrdyrt.inf@gmail.com>
2024-01-08 11:25:32 +0100

    Update classes no 3

---
+++ classes_no_3/Ex_teams/Ex1.py
@@ -0,0 +1,10 @@
+import re
+
+passwords = ['Apple34!rose', 'My87hou#4$', 'abc123', 'Applerose']
+pattern = re.compile(r'^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[*$%!&.]).{8,20}$')
+
+for password in passwords:
+    if pattern.match(password):
+        print(f"{password} - Correct password")
+    else:
+        print(f"{password} - Uncorrect password")
